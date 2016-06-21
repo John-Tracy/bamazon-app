0c73d6345107fc6168d0c16021ee00e156a91784
John Tracy <johnmtracy133@hotmail.com>
2016-06-21 00:49:27 -0400

    options 1 and 2 complete

--- bamazonManager.js
+++ bamazonManager.js
@@ -13,8 +13,50 @@ var connection = mysql.createConnection({
 	database: 'Bamazon'
 
 });
+// =================================================================
 
-var questionList = [
+var direct = {
+
+	
+	viewQuery: 'SELECT ItemID, ProductName, Price, StockQuantity FROM Products',
+	lowQuery: 'SELECT ItemID, ProductName, StockQuantity FROM Products WHERE StockQuantity < 5',
+	addQuestion: []
+	initConnect: function(){
+		connection.connect(function(err){
+			if(err) throw err;
+		});
+	},
+	endConnect: function(){
+		connection.end();
+	},
+	optOne: function(){
+		connection.query(direct.viewQuery, function(err, data){
+			for(var i = 0; i < data.length; i++){
+				console.log('--------------------');
+				console.log('Product ID#: ' + data[i].ItemID);
+				console.log('Name :' + data[i].ProductName);
+				console.log('Price: $' + data[i].Price);
+				console.log('Quantity: ' + data[i].StockQuantity);
+			}
+			promptList();
+		});
+	},
+	optTwo: function(){
+		connection.query(direct.lowQuery, function(err, data){
+			for(var i = 0; i < data.length; i++){
+				console.log('-----Low Inventory-----');
+				console.log('Product ID#: ' + data[i].ItemID);
+				console.log('Name :' + data[i].ProductName);
+				console.log('Quantity: ' + data[i].StockQuantity);
+			}
+			promptList();
+		});
+	}
+}
+
+// =================================================================
+
+var questionList = [ // questions for inquirer function
 
 	{
 		type: 'list',
@@ -30,25 +72,29 @@ var questionList = [
 
 ];
 
-inquirer.prompt(questionList).then(function(answers){
-	userInput = answers.answer;
-	
+var promptList = function(){
 
-	switch(userInput){
+		inquirer.prompt(questionList).then(function(answers){
+			userInput = answers.answer;
+			
 
-		case 'View Products for Sale': 
-			console.log('case 1 worked');
-			break;
-		case 'View Low Inventory':
-			console.log('case 2 worked');
-			break;
-		case 'Add to Inventory':
-			console.log('case 3 worked');
-			break;
-		case 'Add New Product':
-			console.log('case 4 worked');
-			break;
+			switch(userInput){
 
-	}
+				case 'View Products for Sale': 
+					direct.optOne();
+					break;
+				case 'View Low Inventory':
+					direct.optTwo();
+					break;
+				case 'Add to Inventory':
+					console.log('case 3 worked');
+					break;
+				case 'Add New Product':
+					console.log('case 4 worked');
+					break;
 
-});
+			}
+
+		});
+};
+promptList();
